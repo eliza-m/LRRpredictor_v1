3c3e5c96abd4b9fcdc2896bb0deb1966c677311b
eliza-m <elizamartinc@yahoo.com>
2020-02-27 21:13:23 +0200

    Update paths

--- download-validation-set.sh
+++ download-validation-set.sh
@@ -1,11 +1,11 @@
-cd /LRRpredictor_v1/crossValidation/
+cd /home/test/LRRpredictor_v1/crossValidation/
 wget old.biochim.ro/ib/departments/strbiochem/LRRpred/CrossValidation_pkls.tar.gz
 tar -xzf CrossValidation_pkls.tar.gz
 mv CrossValidation_pkls/* ./
 rm CrossValidation_pkls.tar.gz
 rmdir CrossValidation_pkls
 
-cd /LRRpredictor_v1/test/
+cd /home/test/LRRpredictor_v1/test/
 wget old.biochim.ro/ib/departments/strbiochem/LRRpred/test_pkls.tar.gz
 tar -xzf test_pkls.tar.gz
 mv test_pkls/* ./
